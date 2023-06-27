b425a6daffa9c33c0c28249b49e20748f2826886
Manuel Zambrano <89891295+manuelz8@users.noreply.github.com>
2023-06-27 15:47:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$('.about-img').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    fade: true,
+    cssEase: 'linear',
+    adaptiveHeight: true,
+    autoplay: true
+  });
